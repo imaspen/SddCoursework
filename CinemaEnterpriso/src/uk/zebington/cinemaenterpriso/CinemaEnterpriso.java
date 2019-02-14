8cd2e865d3ae876233f9ff4dcad391849268dfcf
Aspen <zebington@gmail.com>
2019-02-14 15:03:37 +0000

    added movie list fxml and paul blart

--- CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/CinemaEnterpriso.java
+++ CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/CinemaEnterpriso.java
@@ -6,19 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import uk.zebington.cinemaenterpriso.controllers.MovieDetailsController;
+import uk.zebington.cinemaenterpriso.controllers.MovieListController;
 import uk.zebington.cinemaenterpriso.controllers.TakesData;
 import uk.zebington.cinemaenterpriso.entities.Movie;
+
+import java.util.ArrayList;
 
 public class CinemaEnterpriso extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         System.setProperty("prism.allowhidpi", "true");
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("boundaries/movieListItem.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("boundaries/movieList.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
-        loader.<TakesData<Movie>>getController().initData(Movie.THE_MATRIX);
+        loader.<MovieListController>getController().initData(Movie.THE_MATRIX, Movie.PAUL_BLART_2);
         primaryStage.show();
     }
 
--- CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/entities/Movie.java
+++ CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/entities/Movie.java
@@ -5,6 +5,7 @@ package uk.zebington.cinemaenterpriso.entities;
  */
 public class Movie {
     public static final Movie THE_MATRIX = new Movie("The Matrix", "15", "Thomas A. Anderson is a man living two lives. By day he is an average computer programmer and by night a hacker known as Neo. Neo has always questioned his reality, but the truth is far beyond his imagination. Neo finds himself targeted by the police when he is contacted by Morpheus, a legendary computer hacker branded a terrorist by the government. Morpheus awakens Neo to the real world, a ravaged wasteland where most of humanity have been captured by a race of machines that live off of the humans' body heat and electrochemical energy and who imprison their minds within an artificial reality known as the Matrix. As a rebel against the machines, Neo must return to the Matrix and confront the agents: super-powerful computer programs devoted to snuffing out Neo and the entire human rebellion.", "Action");
+    public static final Movie PAUL_BLART_2 = new Movie("Paul Blart: Mall Cop 2", "PG", "This movie is bad. Don't watch it.", "\"Comedy\"");
     private String title;
     private String ageRating;
     private String description;
