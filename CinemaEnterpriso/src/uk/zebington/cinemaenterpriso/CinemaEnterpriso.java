14458bad3379c5f3026872e92796e36e362642a6
Aspen <zebington@gmail.com>
2019-02-14 13:24:59 +0000

    added movie details page and controller

---
+++ CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/CinemaEnterpriso.java
@@ -0,0 +1,28 @@
+package uk.zebington.cinemaenterpriso;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import uk.zebington.cinemaenterpriso.controllers.MovieDetailsController;
+import uk.zebington.cinemaenterpriso.entities.Movie;
+
+public class CinemaEnterpriso extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.setProperty("prism.allowhidpi", "true");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("boundaries/MovieDetails.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        loader.<MovieDetailsController>getController().initData(Movie.THE_MATRIX);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/controllers/MovieDetailsController.java
@@ -0,0 +1,21 @@
+package uk.zebington.cinemaenterpriso.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import uk.zebington.cinemaenterpriso.entities.Movie;
+
+public class MovieDetailsController {
+    private Movie movie;
+
+    public Label movieTitle;
+    public Label movieDescription;
+    public ImageView movieRating;
+
+    public void initData(Movie movie) {
+        this.movie = movie;
+        this.movieTitle.setText(movie.getTitle());
+        this.movieDescription.setText(movie.getDescription());
+        this.movieRating.setImage(new Image("/" + movie.getAgeRating() + ".png"));
+    }
+}
