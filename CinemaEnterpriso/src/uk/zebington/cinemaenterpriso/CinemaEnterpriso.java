b518513ec0c5d5af5295cb5037cda2247895cde1
Aspen <zebington@gmail.com>
2019-02-20 12:18:20 +0000

    added icons

--- CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/CinemaEnterpriso.java
+++ CinemaEnterpriso/src/uk/zebington/cinemaenterpriso/CinemaEnterpriso.java
@@ -2,6 +2,7 @@ package uk.zebington.cinemaenterpriso;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import uk.zebington.cinemaenterpriso.controllers.PageContainerController;
 import uk.zebington.cinemaenterpriso.controllers.TheaterListController;
@@ -15,10 +16,18 @@ public class CinemaEnterpriso extends Application {
     @Override
     public void start(Stage primaryStage) {
         System.setProperty("prism.allowhidpi", "true");
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Cinema Enterpiso");
+        addIcons(primaryStage);
         primaryStage.setScene(new Scene(PageContainerController.MAIN_PAGE.getParent()));
         primaryStage.setMaximized(true);
         PageContainerController.MAIN_PAGE.loadNewPage(new TheaterListController(Theater.OA4_01, Theater.SJG_38));
         primaryStage.show();
+    }
+
+    private void addIcons(Stage stage) {
+        String[] resolutions = {"16", "32", "48", "64", "96", "128", "256"};
+        for (String resolution : resolutions) {
+            stage.getIcons().add(new Image("icons/CinemaEnterpriso" + resolution + ".png"));
+        }
     }
 }
